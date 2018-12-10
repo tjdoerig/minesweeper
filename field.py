b94788074ba587eec4a4061c2a8ce3f0d7d085ea
Thomas Dörig <thomas@thomass-macbook-pro-1.home>
2018-12-10 21:44:57 +0100

    class erstellt

--- field.py
+++ field.py
@@ -1,24 +1,26 @@
 import tkinter as tk
 
+class field():
+    def __init__(self):
 
-#LeftClick to discover field
-def leftClick(event):
-    print("leftClick")
-    return
+        #LeftClick to discover field
+        def leftClick(event):
+            print("leftClick")
+            return
 
-#RighClick to set flag
-def rightClick(event):
-    b["image"] =  flag
+        #RighClick to set flag
+        def rightClick(event):
+            b["image"] =  flag
 
-root = tk.Tk()
+        root = tk.Tk()
 
-#import images
-flag = tk.PhotoImage(file="FlagScale.png")
-#bomb = tk.PhotoImage(file="Bomb.png")
+        #import images
+        flag = tk.PhotoImage(file="FlagScale.png")
+        #bomb = tk.PhotoImage(file="Bomb.png")
 
-b = tk.Button(root, image = flag, height = 5, width = 5)
-b.bind("<Button-1>", leftClick)
-b.bind("<Button-2>", rightClick)
-b.pack()
+        b = tk.Button(root, image = flag, height = 5, width = 5)
+        b.bind("<Button-1>", leftClick)
+        b.bind("<Button-2>", rightClick)
+        b.pack()
 
-root.mainloop()
+        root.mainloop()
